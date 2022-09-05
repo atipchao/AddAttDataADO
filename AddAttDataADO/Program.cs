64dc0469ccf04084fd08d963b54995acc85607df
atipchao <atip.chaogunhar@cookcountyil.gov>
2022-09-05 16:09:51 -0500

    4 nd commit

--- AddAttDataADO/Program.cs
+++ AddAttDataADO/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
+//using System.Data.SqlClient;
 using System.IO;
 using System.Linq;
 using System.Text;
